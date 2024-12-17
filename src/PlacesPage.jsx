737be4c5e4d2368cd148191c0dd2ed3e7c631919
Kom22910 <certificate8806799065@gmail.com>
2024-12-17 15:02:36 +0530

    third changes

--- src/PlacesPage.jsx
+++ src/PlacesPage.jsx
@@ -740,7 +740,7 @@ const PlacesPage = () => {
                                 {/* 2 - add more place  */}
 
                                 <div className="col-12 px-2 py-2 mt-5 text-center">
-                                    <button className='btn btn-success w-25 py-2' onClick={() => setAddmore(true)} > Add Your Food </button>
+                                    <button className='btn btn-success col-sm-3 col-6 py-2' onClick={() => setAddmore(true)} > Add Your Resautrants </button>
                                 </div>
 
                             </div>
